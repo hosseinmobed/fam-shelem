fdf37cdffecc6b99048325f8af766059afd16e44
hosseinmobed <74431805+hosseinmobed@users.noreply.github.com>
2021-10-14 13:27:00 +0330

    Add files via upload

---
+++ aServer.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const http = require("http");
+
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const file = fs.readFileSync("./fourOfour.html");
+
+app.use(express.static(__dirname + "/aPublic"));
+
+app.get("*", (req, res) => {
+  res.send(file.toString());
+});
+
+const uts = {};
+const itu = {};
+
+io.on("connection", socket => {
+  socket.on("reg", userName => {
+    uts[userName] = socket;
+    itu[socket.id] = userName;
+  });
+  socket.on("send", ({ to, msg }) => {
+    uts[to].emit("what", itu[socket.id] + ": " + msg);
+  });
+});
+
+const port = 3005;
+server.listen(port, () => {
+  console.log("listening on *: " + port);
+});
